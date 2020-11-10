ab1569cd22dfda224f79e3141f49d6c92b84ee9b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-10 14:25:24 -0300

    profile update view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import SignupPageView
+from users.views import ProfileEditView
 
 urlpatterns = [
-    path('signup/', SignupPageView.as_view(), name='signup'),
+    path('update/', ProfileEditView.as_view(), name='profile_edit'),
 ]
