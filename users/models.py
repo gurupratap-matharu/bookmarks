2512e31b82074a1c474f6c3bd16337643cf14d16
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-10 14:25:14 -0300

    profile model updated with photo

--- users/models.py
+++ users/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
 
     def __str__(self):
