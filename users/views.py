3b3073aacfa2189d5607d5d2cc94cf20b673180f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-10 14:43:44 -0300

    profile detail view added

--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
+from django.views.generic import DetailView, UpdateView
 
-from users.forms import ProfileEditForm
+from users.forms import ProfileUpdateForm
+from users.models import Profile
 
 
-class ProfileEditView(LoginRequiredMixin, UpdateView):
-    form_class = ProfileEditForm
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    model = Profile
+    template_name = 'users/profile_detail.html'
+    context_object_name = 'profile'
+
+
+class ProfileUpdate(LoginRequiredMixin, UpdateView):
+    form_class = ProfileUpdateForm
     success_url = reverse_lazy('home')
-    template_name = 'profile_edit.html'
+    template_name = 'users/profile_.html'
