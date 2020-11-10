c096ef748d80944f6edc83d74aea8111cdd56ef8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-10 14:25:36 -0300

    using generic update view for profile model

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import UpdateView
 
-from .forms import CustomUserCreationForm
+from users.forms import ProfileEditForm
 
 
-class SignupPageView(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+class ProfileEditView(LoginRequiredMixin, UpdateView):
+    form_class = ProfileEditForm
+    success_url = reverse_lazy('home')
+    template_name = 'profile_edit.html'
