69ebb2201c185cea71c85a831edbf8101307a56c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-10 14:43:20 -0300

    added signals to create profile automatically

--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ import uuid
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.urls import reverse
 
 
@@ -23,3 +25,14 @@ class Profile(models.Model):
 
     def get_absolute_url(self):
         return reverse('profile_detail', args=[str(self.id)])
+
+
+@receiver(post_save, sender=get_user_model())
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=get_user_model())
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
