7eef0fb8a3ee620a1080d93bcc37a5a260e918ed
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-10 14:25:03 -0300

    added profile edit model form

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,23 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
+from users.models import Profile
+
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta:
         model = get_user_model()
         fields = ('email', 'username')
 
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = get_user_model()
         fields = ('email', 'username')
+
+
+class ProfileEditForm(forms.Form):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
