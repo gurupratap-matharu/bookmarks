04fffb9026d4df4f8f9518be6225702dbde1d507
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-22 12:31:24 -0300

    added create view for image model

--- images/views.py
+++ images/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView
+
+from images.forms import ImageCreateForm
+
+logger = logging.getLogger(__name__)
+
+
+class ImageCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    form_class = ImageCreateForm
+    template_name = 'images/image_form.html'
+    success_message = "%(title) successfully created"
+
+    def form_valid(self, form):
+        logger.info('form_valid: assigning user...')
+        form.instance.user = self.request.user
+        return super().form_valid(form)
