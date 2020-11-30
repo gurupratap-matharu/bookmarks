11a8bb2503c2edff9b8212702895b5c6ad1d35f0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-29 22:23:05 -0300

    createview and detailview implemented

--- images/views.py
+++ images/views.py
@@ -3,18 +3,28 @@ import logging
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
+from django.views.generic.detail import DetailView
 
 from images.forms import ImageCreateForm
+from images.models import Image
 
 logger = logging.getLogger(__name__)
 
 
 class ImageCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Image
     form_class = ImageCreateForm
     template_name = 'images/image_form.html'
     success_message = "%(title) successfully created"
 
     def form_valid(self, form):
         logger.info('form_valid: assigning user...')
+        print('veer form is valid...')
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class ImageDetailView(LoginRequiredMixin, DetailView):
+    model = Image
+    template_name = 'images/image_detail.html'
+    context_object_name = 'image'
