d8df3d821f69a7d6886972be4863f309de61368c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-29 22:23:25 -0300

    made the url field unhidden for now

--- images/forms.py
+++ images/forms.py
@@ -1,3 +1,4 @@
+import logging
 from urllib import request
 
 from django import forms
@@ -6,19 +7,25 @@ from django.utils.text import slugify
 
 from images.models import Image
 
+logger = logging.getLogger(__name__)
+
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('title', 'url', 'description')
         widgets = {
-            'url': forms.HiddenInput,
+            # 'url': forms.HiddenInput,
+            'description': forms.Textarea(attrs={'cols': 80, 'rows': 5}),
         }
 
     def clean_url(self):
+        print('veer cleaning url...')
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg']
         extension = url.rsplit('.', 1)[1].lower()
+        logger.info('clean_url: extension: %s', extension)
+
         if extension not in valid_extensions:
             raise forms.ValidationError('The given url does not match valid '
                                         'image extensions.')
@@ -30,11 +37,17 @@ class ImageCreateForm(forms.ModelForm):
         extension = image_url.rsplit('.', 1)[1].lower()
         name = slugify(obj.title)
         image_name = f'{name}.{extension}'
+        logger.info('ImageCreateForm: save(): image_name: %s...', image_name)
 
         # download the image from the given url
         response = request.urlopen(image_url)
+
         obj.image.save(image_name, ContentFile(response.read()), save=False)
+
+        logger.info('ImageCreateForm: image downloaded successfully...')
 
         if commit:
             obj.save()
+            logger.info('ImageCreateForm: obj saved...')
+
         return obj
