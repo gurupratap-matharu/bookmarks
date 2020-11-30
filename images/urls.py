7a9cba5ff98c587c66ae901abf90e000d94913fd
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-29 22:23:42 -0300

    added a new endpoint for detailview

--- images/urls.py
+++ images/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from images.views import ImageCreateView
+from images.views import ImageCreateView, ImageDetailView
 
 app_name = 'images'
 
-url_patterns = [
+urlpatterns = [
     path('create/', ImageCreateView.as_view(), name='create'),
+    path('<slug:slug>/', ImageDetailView.as_view(), name='image_detail'),
 ]
