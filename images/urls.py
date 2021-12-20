8f1d614c4cd2ea13bc9e78264cd7c1a0f363689d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-20 14:26:36 +0530

    format using black

--- images/urls.py
+++ images/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from images.views import ImageCreateView, ImageDetailView
 
-app_name = 'images'
+app_name = "images"
 
 urlpatterns = [
-    path('create/', ImageCreateView.as_view(), name='create'),
-    path('<slug:slug>/', ImageDetailView.as_view(), name='image_detail'),
+    path("create/", ImageCreateView.as_view(), name="create"),
+    path("<slug:slug>/", ImageDetailView.as_view(), name="image_detail"),
 ]
