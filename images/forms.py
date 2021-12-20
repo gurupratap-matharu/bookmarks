0fc2f1ec12de42a311115bf4f5df1100b1f69109
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-20 14:26:21 +0530

    ImageCreateview implemented using model form

--- images/forms.py
+++ images/forms.py
@@ -13,41 +13,41 @@ logger = logging.getLogger(__name__)
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = ('title', 'url', 'description')
+        fields = ("title", "url", "description")
         widgets = {
-            # 'url': forms.HiddenInput,
-            'description': forms.Textarea(attrs={'cols': 80, 'rows': 5}),
+            "url": forms.HiddenInput,
+            "description": forms.Textarea(attrs={"cols": 80, "rows": 5}),
         }
 
     def clean_url(self):
-        print('veer cleaning url...')
-        url = self.cleaned_data['url']
-        valid_extensions = ['jpg', 'jpeg']
-        extension = url.rsplit('.', 1)[1].lower()
-        logger.info('clean_url: extension: %s', extension)
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg"]
+        extension = url.rsplit(".", 1)[1].lower()
+        logger.info("clean_url: extension: %s", extension)
 
         if extension not in valid_extensions:
-            raise forms.ValidationError('The given url does not match valid '
-                                        'image extensions.')
+            raise forms.ValidationError(
+                "The given url does not match valid image extensions."
+            )
         return url
 
     def save(self, force_insert=False, force_update=False, commit=True):
         obj = super().save(commit=False)
-        image_url = self.cleaned_data['url']
-        extension = image_url.rsplit('.', 1)[1].lower()
+        image_url = self.cleaned_data["url"]
+        extension = image_url.rsplit(".", 1)[1].lower()
         name = slugify(obj.title)
-        image_name = f'{name}.{extension}'
-        logger.info('ImageCreateForm: save(): image_name: %s...', image_name)
+        image_name = f"{name}.{extension}"
+        logger.info("ImageCreateForm: save(): image_name: %s...", image_name)
 
         # download the image from the given url
         response = request.urlopen(image_url)
 
         obj.image.save(image_name, ContentFile(response.read()), save=False)
 
-        logger.info('ImageCreateForm: image downloaded successfully...')
+        logger.info("ImageCreateForm: image downloaded successfully...")
 
         if commit:
             obj.save()
-            logger.info('ImageCreateForm: obj saved...')
+            logger.info("ImageCreateForm: obj saved...")
 
         return obj
--- images/views.py
+++ images/views.py
@@ -14,17 +14,26 @@ logger = logging.getLogger(__name__)
 class ImageCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Image
     form_class = ImageCreateForm
-    template_name = 'images/image_form.html'
-    success_message = "%(title) successfully created"
+    template_name = "images/image_form.html"
+    success_message = "%(title)s successfully created"
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        if self.request.method == "GET":
+            kwargs.update(
+                {
+                    "data": self.request.GET,
+                }
+            )
+        return kwargs
 
     def form_valid(self, form):
-        logger.info('form_valid: assigning user...')
-        print('veer form is valid...')
+        logger.info("form_valid: assigning user...")
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 
 class ImageDetailView(LoginRequiredMixin, DetailView):
     model = Image
-    template_name = 'images/image_detail.html'
-    context_object_name = 'image'
+    template_name = "images/image_detail.html"
+    context_object_name = "image"
