48278f487e859981e17b68cd20b38425865b3217
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-20 14:25:30 +0530

    Remove boilerplate introduction

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,3 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -20,16 +5,13 @@ from django.urls import include, path
 
 urlpatterns = [
     # Django admin
-    path('dj-admin/', admin.site.urls),
-
+    path("dj-admin/", admin.site.urls),
     # User management
-    path('accounts/', include('allauth.urls')),
-
+    path("accounts/", include("allauth.urls")),
     # Local
-    path('', include('pages.urls')),
-    path('profile/', include('users.urls')),
-    path('images/', include('images.urls', namespace='images')),
-
+    path("", include("pages.urls")),
+    path("profile/", include("users.urls")),
+    path("images/", include("images.urls", namespace="images")),
 ]
 
 if settings.DEBUG:
